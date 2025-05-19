6af7cfdc5dbd83664dde70524a1f0107f6abe8f2
tonytani37 <vka77606@gmail.com>
2025-05-20 08:23:54 +0900

    2025/05/20  8:23

--- script.js
+++ script.js
@@ -5,19 +5,7 @@ fetch('data/nogizaka_profile.json')
     const list = document.getElementById('memberList');
 
     members.forEach((member, index) => {
-      // const li = document.createElement('li');
-
-      // const link = document.createElement('a');
-      // link.href = `member.html?index=${index}`;
-      // link.textContent = member.名前;
-      // // link.className = 'member-link';
-
-      // const yomi = document.createElement('span');
-      // yomi.className = 'yomigana';
-      // yomi.textContent = `（${member.よみがな}）`;
-
       const tr = document.createElement('tr');
-
       const nameTd = document.createElement('td');
       const link = document.createElement('a');
       link.href = `member.html?index=${index}`;
@@ -25,11 +13,9 @@ fetch('data/nogizaka_profile.json')
       // link.className = 'body-index';
       nameTd.appendChild(link);
 
-
       const yomiTd = document.createElement('td');
       yomiTd.className = 'yomigana';
       yomiTd.textContent = "　　("+member.よみがな+")";
-
 
       list.appendChild(nameTd);
       list.appendChild(yomiTd);
